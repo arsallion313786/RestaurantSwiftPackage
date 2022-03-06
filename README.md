1118d155bbd23d908791584574ad0a894e7e2379
arsallion313786 <99034072+arsallion313786@users.noreply.github.com>
2022-03-06 15:26:38 +0500

    Add information in Readme

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # RestaurantSwiftPackage
 
-A description of this package.
+Its a Restaurant Listing Package
+
+
+Example:-
+In a Example Folder, created example project. This project will help you how you can use this package for listing Restaurants
+
+Dependencies:-
+ 1. SnapKit
+ 2. SDWebImage
+ 
+ 
+ SnapKit:-
+ https://github.com/SnapKit/SnapKit
+ 
+ SDWebImage
+ https://github.com/SDWebImage/SDWebImage
--- Tests/RestaurantSwiftPackageTests/RestaurantSwiftPackageTests.swift
+++ Tests/RestaurantSwiftPackageTests/RestaurantSwiftPackageTests.swift
@@ -2,10 +2,20 @@ import XCTest
 @testable import RestaurantSwiftPackage
 
 final class RestaurantSwiftPackageTests: XCTestCase {
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         XCTAssertEqual(RestaurantSwiftPackage().text, "Hello, World!")
+    }
+    
+    
+    func testRestaurantListCount() async throws {
+        
+        let repo = RestaurantRepo();
+        let result = try await repo.getRestaurants()
+        
+        XCTAssertTrue(result.count > 0, "Something went wrong")
     }
 }
